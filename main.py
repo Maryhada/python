482c7950139288975264a7e46d75ea37a1768a59
Treasure <treasure.olanipekun@gmail.com>
2025-10-25 19:24:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is how to comment using python.
+print("Welcome to Python programming!")
+# This line is going to be about me.
+print("My name is Treasure Olanipekun.")
+print("i love coding, sports, reading, dancing, acting and singing.")
+print("I am 11 years old.")
+print("I am from Nigeria.")
+print("I am in grade 8.") 
